8dbb8355ce2d23ad7e545e51be57c46a8543a7d9
Ivan Gomes <ivanjunior.pt@gmail.com>
2021-07-16 15:30:13 -0300

    Finalização da tela do chat

---
+++ styled-whatslab/styled-whatslab/src/components/Container.js
@@ -0,0 +1,10 @@
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+   width: 30vw;
+   height: 100vh;
+   border: 1px solid black;
+`
+
+export default Container
---
+++ styled-whatslab/styled-whatslab/src/components/MainContainer.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+import styled from 'styled-components';
+
+
+ const MainContainer = styled.div`
+     display: flex;
+     justify-content: center;
+     align-items: center;
+   
+`
+
+export default MainContainer
